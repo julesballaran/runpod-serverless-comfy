a4bab614df95870700eb2b7c1b7e25bfb2722e1f
Jules Ballaran <julesballaran@gmail.com>
2024-03-07 18:05:09 +0800

    inital commit

---
+++ src/rp_handler.py
@@ -0,0 +1,151 @@
+import runpod
+# from runpod.serverless.utils import rp_upload
+import json
+import urllib.request
+import urllib.parse
+import time
+import os
+import requests
+# import base64
+# from io import BytesIO
+
+
+COMFY_API_AVAILABLE_INTERVAL_MS = 50
+# Maximum number of API check attempts
+COMFY_API_AVAILABLE_MAX_RETRIES = 500
+# Time to wait between poll attempts in milliseconds
+COMFY_POLLING_INTERVAL_MS = 250
+# Maximum number of poll attempts
+COMFY_POLLING_MAX_RETRIES = 500
+# Host where ComfyUI is running
+COMFY_HOST = "127.0.0.1:8188"
+# Enforce a clean state after each job is done
+# see https://docs.runpod.io/docs/handler-additional-controls#refresh-worker
+REFRESH_WORKER = os.environ.get("REFRESH_WORKER", "false").lower() == "true"
+
+
+def validate_input(job_input):
+    if job_input is None:
+        return None, "Please provide input"
+    
+    if isinstance(job_input, str):
+        try:
+            job_input = json.loads(job_input)
+        except json.JSONDecodeError:
+            return None
+    
+    workflow = job_input.get("workflow")
+    if workflow is None:
+        return None
+    
+    return workflow
+
+
+def check_server(url, retries=50, delay=500):
+    """
+    Check if a server is reachable via HTTP GET request
+
+    Args:
+    - url (str): The URL to check
+    - retries (int, optional): The number of times to attempt connecting to the server. Default is 50
+    - delay (int, optional): The time in milliseconds to wait between retries. Default is 500
+
+    Returns:
+    bool: True if the server is reachable within the given number of retries, otherwise False
+    """
+
+    for i in range(retries):
+        try:
+            response = requests.get(url)
+
+            # If the response status code is 200, the server is up and running
+            if response.status_code == 200:
+                print(f"runpod-worker-comfy - API is reachable")
+                return True
+        except requests.RequestException as e:
+            # If an exception occurs, the server may not be ready
+            pass
+
+        # Wait for the specified delay before retrying
+        time.sleep(delay / 1000)
+
+    print(
+        f"runpod-worker-comfy - Failed to connect to server at {url} after {retries} attempts."
+    )
+    return False
+
+
+def queue_workflow(workflow):
+    """
+    Queue a workflow to be processed by ComfyUI
+
+    Args:
+        workflow (dict): A dictionary containing the workflow to be processed
+
+    Returns:
+        dict: The JSON response from ComfyUI after processing the workflow
+    """
+
+    # The top level element "prompt" is required by ComfyUI
+    data = json.dumps({"prompt": workflow}).encode("utf-8")
+
+    req = urllib.request.Request(f"http://{COMFY_HOST}/prompt", data=data)
+    return json.loads(urllib.request.urlopen(req).read())
+
+
+def get_history(prompt_id):
+    """
+    Retrieve the history of a given prompt using its ID
+
+    Args:
+        prompt_id (str): The ID of the prompt whose history is to be retrieved
+
+    Returns:
+        dict: The history of the prompt, containing all the processing steps and results
+    """
+    with urllib.request.urlopen(f"http://{COMFY_HOST}/history/{prompt_id}") as response:
+        return json.loads(response.read())
+
+
+def handler(job):
+    job_input = job["input"]
+    workflow = validate_input(job_input)
+    if not workflow:
+        return {"error": "invalid workflow"}
+    
+    check_server(
+        f"http://{COMFY_HOST}",
+        COMFY_API_AVAILABLE_MAX_RETRIES,
+        COMFY_API_AVAILABLE_INTERVAL_MS,
+    )
+
+    try:
+        queued_workflow = queue_workflow(workflow)
+        prompt_id = queued_workflow["prompt_id"]
+        print(f"runpod-worker-comfy - queued workflow with ID {prompt_id}")
+    except Exception as e:
+        return {"error": f"Error queuing workflow: {str(e)}"}
+    
+    print(f"runpod-worker-comfy - wait until image generation is complete")
+    retries = 0
+    try:
+        while retries < COMFY_POLLING_MAX_RETRIES:
+            history = get_history(prompt_id)
+
+            # Exit the loop if we have found the history
+            if prompt_id in history and history[prompt_id].get("outputs"):
+                break
+            else:
+                # Wait before trying again
+                time.sleep(COMFY_POLLING_INTERVAL_MS / 1000)
+                retries += 1
+        else:
+            return {"error": "Max retries reached while waiting for image generation"}
+    except Exception as e:
+        return {"error": f"Error waiting for image generation: {str(e)}"}
+    
+    return history[prompt_id].get("outputs")
+
+
+if __name__ == "__main__":
+    runpod.serverless.start({"handler": handler})
